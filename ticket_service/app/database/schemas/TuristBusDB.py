0db8fd663e52e2717f087da1aefcce41c58da214
PanzerKnaker <47358616+PanzerKnaker@users.noreply.github.com>
2024-02-14 21:06:49 +0300

    CICD3

--- ticket_service/app/database/schemas/TuristBusDB.py
+++ ticket_service/app/database/schemas/TuristBusDB.py
@@ -6,7 +6,7 @@ class TuristBus(Base):
     __tablename__ = 'TuristBus'
 
     id = Column(Integer, primary_key=True, index=True)
-    model = Column(String, nullable=False)
+    modelbus = Column(String, nullable=False)
     destination = Column(String, nullable=False)
     datego = Column(DateTime, nullable=False)
     seats = Column(Integer, nullable=False)
--- ticket_service/app/main.py
+++ ticket_service/app/main.py
@@ -1,16 +1,41 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+import os
 from typing import Annotated
-from app.database.schemas.TripTicketDB import TripTicket
-from app.database.schemas.TuristBusDB import TuristBus
-from app.database.database import engine, SessionLocal
+from database.schemas.TripTicketDB import TripTicket
+from database.schemas.TuristBusDB import TuristBus
+from database.database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from app.database import database as database
+import uvicorn
+from fastapi import FastAPI, Depends, HTTPException, status, Form
+from database import database as database
 import smtplib
 from email.mime.text import MIMEText
 
 app = FastAPI()
 database.Base.metadata.create_all(bind=engine)
 
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import  TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from keycloak import KeycloakOpenID
+
+from prometheus_fastapi_instrumentator import Instrumentator
+Instrumentator().instrument(app).expose(app)
+
+
+KEYCLOAK_URL = "http://keycloak:8080/"
+KEYCLOAK_CLIENT_ID = "koshkin"
+KEYCLOAK_REALM = "koshkinrealm"
+KEYCLOAK_CLIENT_SECRET = "wIYhDqHAudxL6xtcp8zrdRk0yDnw1fuo"
+
+keycloak_openid = KeycloakOpenID(server_url=KEYCLOAK_URL,
+                                  client_id=KEYCLOAK_CLIENT_ID,
+                                  realm_name=KEYCLOAK_REALM,
+                                  client_secret_key=KEYCLOAK_CLIENT_SECRET)
+
+user_token =""
 
 def get_db():
     db = SessionLocal()
@@ -23,9 +48,39 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+@app.post("/get-token")
+async def get_token(username: str = Form(...), password: str = Form(...)):
+    try:
+        # Получение токена
+        token = keycloak_openid.token(grant_type=["password"],
+                                      username=username,
+                                      password=password)
+        global user_token
+        user_token = token
+        return token
+    except Exception as e:
+        print(e)  # Логирование для диагностики
+        raise HTTPException(status_code=400, detail="Не удалось получить токен")
+
+def check_user_roles():
+    global user_token
+    token = user_token
+    try:
+        userinfo = keycloak_openid.userinfo(token["access_token"])
+        token_info = keycloak_openid.introspect(token["access_token"])
+        if "testRole" not in token_info["realm_access"]["roles"]:
+            raise HTTPException(status_code=403, detail="Access denied")
+        return "Autorization success!"
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token or access denied")
+
 @app.get("/alive", status_code=status.HTTP_200_OK)
 async def ticket_alive():
-    return {'message' : 'service alive'}
+    if (check_user_roles()):
+        return {'message' : 'service alive'}
+    else:
+        return C
+
 
 @app.get("/tripticket/{tripticket_id}")
 async def fetch_tickets(tripticket_id: int, db: db_dependency):
@@ -37,10 +92,12 @@ async def fetch_tickets(tripticket_id: int, db: db_dependency):
 
 
 def check_turistbus_existence(direction: str, db: db_dependency):
-    return db.query(TuristBus).filter(
-        TuristBus.direction == direction
-    ).first()
-
+    if (check_user_roles()):
+        return db.query(TuristBus).filter(
+            TuristBus.direction == direction
+        ).first()
+    else:
+        return "Wrong User Token"
 
 
 @app.post("/buy_tripticket")
@@ -66,3 +123,5 @@ async def buy_tripticket(direction: str,
     except HTTPException as e:
         print(f"Error: {e}")
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 80)))
